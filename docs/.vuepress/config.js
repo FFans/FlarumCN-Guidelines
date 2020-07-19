10742368898cfa629c6da76f808277b78ad02c16
Littlegolden <littlegoldenjin@gmail.com>
2020-07-19 21:34:12 +0800

    规范更新

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,32 +4,36 @@ const secret = require("./config/secret");
 module.exports = {
     base: '/docs/',
     plugins: pluginsConfig,
-    lang: 'zh-Hans',
-    title: '社区文档',
+    locales: {
+      '/':{
+        lang: 'zh-Hans',
+        title: '社区指南'
+      }
+    },
     head:  [
         ['meta', { name: 'author', content: 'FlarumCN'}],
-        ['meta', { name: 'keywords', content: 'Flarum 中文社区, FlarumCN, 社区文档'}],
+        ['meta', { name: 'keywords', content: 'Flarum 中文社区, FlarumCN, 社区文档, 社区指南'}],
         ['link', { rel: 'icon', href: '/assets/img/ico.png' }],
     ],
 
     themeConfig: {
-      logo: '/assets/img/logo.webp',
+      // logo: '/assets/img/logo.webp',
       smoothScroll: true,
       nav: [
         { text: '指南', link: '/protocol.html' },
-        { text: '主站', link: 'https://www.discuss.flarum.org/' },
+        { text: '主站', link: 'https://www.flarum.org.cn/' },
         { text: '社区', link: 'https://discuss.flarum.org.cn/', target:'_self', rel:''  },
       ],
       sidebar: [
         {
-          title: '第一章：用户协议',
+          title: '第一部分：用户协议',
           collapsable: false, // 展开侧边栏分组
           children: [
             'protocol'
           ]
         },
       ],
-      lastUpdated: '上次更新：',
+      lastUpdated: '上次更新',
       docsRepo: 'https://gitee.com/FlarumCN/Guidelines',
       docsDir: 'docs',
       editLinks: true,
