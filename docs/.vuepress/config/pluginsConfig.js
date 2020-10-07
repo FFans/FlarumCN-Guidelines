ab133cee27fba785d04bc544d0f09f1c7936726c
Littlegolden <35162802+Littlegolden@users.noreply.github.com>
2020-10-07 20:17:43 +0800

    Update

--- docs/.vuepress/config/pluginsConfig.js
+++ docs/.vuepress/config/pluginsConfig.js
@@ -4,14 +4,14 @@ module.exports = {
     '@vuepress/active-header-links':{},
     '@vuepress/back-to-top':{},
     '@vuepress/nprogress':{},
-    'vuepress-plugin-comment':
-    {
-      choosen: 'valine', 
-      // options选项中的所有参数，会传给Valine的配置
-      options: {
-        el: '#valine-vuepress-comment',
-        appId: secret.appId,
-        appKey: secret.appKey
-      }
-    }
+    // 'vuepress-plugin-comment':
+    // {
+    //   choosen: 'valine', 
+    //   // options选项中的所有参数，会传给Valine的配置
+    //   options: {
+    //     el: '#valine-vuepress-comment',
+    //     appId: secret.appId,
+    //     appKey: secret.appKey
+    //   }
+    // }
 }
